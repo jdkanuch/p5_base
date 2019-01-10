1fda3b624a665a90a6d79eaa7448d63f0298590a
Josh Kanuch <jdkanuch@gmail.com>
2019-01-09 19:26:42 -0700

    Blank P5.js project.

---
+++ main.js
@@ -0,0 +1,28 @@
+// Author   : Josh Kanuch
+// Date     : 2019-01-09
+// P5.js template file
+
+// Use to load data
+function preload(){
+    // most awesome way to load json.
+    //data = loadJSON('data/data.json');
+}
+
+// Runs once
+function setup(){
+    var canvas = createCanvas(1200, 800);
+    canvas.parent('content-div');
+
+}
+
+// Runs at frameRate
+function draw() {
+    background(51);
+function draw() {
+    background(51);
+    //graph.simulate();
+    graph.show();
+
+}
+
+}
